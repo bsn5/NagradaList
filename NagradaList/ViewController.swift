2b76a6588316749070c0eabbbfd06d61824ee109
Babster <koksharov.evgenii@gmail.com>
2025-12-01 16:29:47 +0500

    Initial Commit

---
+++ NagradaList/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NagradaList
+//
+//  Created by MACbook on 01.12.2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ NagradaListTests/NagradaListTests.swift
@@ -0,0 +1,17 @@
+//
+//  NagradaListTests.swift
+//  NagradaListTests
+//
+//  Created by MACbook on 01.12.2025.
+//
+
+import Testing
+@testable import NagradaList
+
+struct NagradaListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
